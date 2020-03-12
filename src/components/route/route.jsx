9ee4b12969ff364ac45daea56798d6af110cbe80
sanjaymajhi <1sanjaymajhi@gmail.com>
2020-03-12 20:58:13 +0530

    create train and route form

--- src/components/route/route.jsx
+++ src/components/route/route.jsx
@@ -4,22 +4,107 @@ class T_Route extends Component {
     super(props);
 
     this.state = {
-      count: 0
+      count: 0,
+      src_stn: "",
+      des_stn: "",
+      stations: [],
+      distance: 0,
+      route_code: ""
     };
   }
 
   countHandler = e => {
-    let select_div = document.getElementById("select_div");
-    select_div.innerHTML = "";
-    const value = e.target.value;
-    for (let i = 0; i < value; i++) {
-      let label = document.createElement("label");
-      label.htmlFor = "station" + i.toString();
-      label.innerHTML = "Station " + (i + 1).toString() + ": ";
-      let select = document.createElement("select");
-      select.name = select.id = "station" + i.toString();
-      select_div.appendChild(label);
-      select_div.appendChild(select);
+    const target = e.target;
+    const url = "/booking/stations/";
+    fetch(url, { method: "GET" })
+      .then(res => res.json())
+      .then(station_list => {
+        let select_div = document.getElementById("select_div");
+        select_div.innerHTML = "";
+        const value = target.value;
+        for (let i = 0; i < value; i++) {
+          let label = document.createElement("label");
+          label.htmlFor = "station" + i.toString();
+          label.innerHTML = "Station " + (i + 1).toString() + ": ";
+          let select = document.createElement("select");
+          select.name = select.id = "station" + i.toString();
+          select.onchange = this.handleChange;
+
+          //adding select selection as disabled and selected option
+          const option = document.createElement("option");
+          option.innerHTML = "Select Station";
+          option.disabled = true;
+          option.selected = true;
+          select.appendChild(option);
+
+          //adding all other options
+          station_list.list.map(station => {
+            const option = document.createElement("option");
+            option.value = station._id;
+            option.innerHTML = station.name + " - " + station.code;
+            select.appendChild(option);
+          });
+
+          select_div.appendChild(label);
+          select_div.appendChild(select);
+        }
+      });
+  };
+
+  submit = async e => {
+    e.preventDefault();
+    await this.setState({
+      src_stn: this.state.stations[0],
+      des_stn: this.state.stations[this.state.stations.length - 1]
+    });
+    const select = document.getElementById("select_div");
+    select.innerHTML = "";
+    const payload = {
+      src_stn: this.state.src_stn,
+      des_stn: this.state.des_stn,
+      stations: this.state.stations,
+      distance: this.state.distance,
+      route_code: this.state.route_code
+    };
+    console.log(payload);
+    fetch("/booking/route/create", {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.saved === "success") {
+          const h2 = document.getElementById("route_saved");
+          h2.style.visibility = "visible";
+          setTimeout(function() {
+            h2.style.visibility = "hidden";
+          }, 5000);
+          this.setState({
+            count: 0,
+            src_stn: "",
+            des_stn: "",
+            stations: [],
+            distance: 0,
+            route_code: ""
+          });
+        }
+      });
+  };
+
+  handleChange = e => {
+    const target = e.target;
+    const name = target.name;
+    const value = target.value;
+    if (name !== "distance" && name !== "route_code") {
+      const stations = this.state.stations;
+      stations.push(value);
+      this.setState({ stations: stations });
+      //Here is an error if selection is done using keyboard not by mouse
+    } else {
+      this.setState({ [name]: value });
     }
   };
 
@@ -27,11 +112,30 @@ class T_Route extends Component {
     return (
       <div>
         <h1>Route Create : </h1>
-        <form action="" id="form">
+        <form id="form" onSubmit={this.submit}>
+          <label htmlFor="route_code">Route Code : </label>
+          <input
+            type="text"
+            name="route_code"
+            id="route_code"
+            onChange={this.handleChange}
+          />
+          <label htmlFor="distance">Distance : </label>
+          <input
+            type="number"
+            name="distance"
+            id="distance"
+            onChange={this.handleChange}
+          />
           <label htmlFor="count">No. of stations : </label>
           <input type="number" name="count" onChange={this.countHandler} />
+
           <div id="select_div"></div>
+          <input type="submit" value="Save" />
         </form>
+        <h1 id="route_saved" style={{ visibility: "hidden" }}>
+          Route saved to database...
+        </h1>
       </div>
     );
   }
--- src/components/train/train.jsx
+++ src/components/train/train.jsx
@@ -1,14 +1,111 @@
 import React, { Component } from "react";
 class Train extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      train_no: 0,
+      available_tiers: [],
+      departing_days: [],
+      route: "",
+      depart_time: "00:00:00",
+      arrival_time: "00:00:00",
+      coach_seats: 0,
+      total_seats: 0,
+      ticket_cost: 0,
+      available_seats: 0,
+      total_coaches: 0
+    };
+    this.loadRoutes();
+  }
+
+  handleChange = e => {
+    const target = e.target;
+    const name = target.name;
+    const value = target.value;
+    if (name !== "available_tiers" && name !== "departing_days") {
+      this.setState({ [name]: value });
+    } else {
+      const arr = this.state[name];
+      arr.push(value);
+      this.setState({ [name]: arr });
+    }
+  };
+
+  loadRoutes = () => {
+    fetch("/booking/routes/", { method: "GET" })
+      .then(res => res.json())
+      .then(data => {
+        const select = document.getElementById("route");
+        const option = document.createElement("option");
+        option.innerHTML = "Select Route";
+        option.disabled = true;
+        option.selected = true;
+        select.appendChild(option);
+        for (let i = 0; i < data.list.length; i++) {
+          const option = document.createElement("option");
+          option.value = data.list[i]._id;
+          option.innerHTML = data.list[i].route_code;
+          select.appendChild(option);
+        }
+      });
+  };
+
+  submit = e => {
+    e.preventDefault();
+    this.setState({ available_seats: this.state.total_seats });
+    const url = "/booking/train/create/";
+    const payload = {
+      ...this.state
+    };
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.saved === "success") {
+          const h2 = document.getElementById("train_saved");
+          h2.style.visibility = "visible";
+          setTimeout(function() {
+            h2.style.visibility = "hidden";
+          }, 5000);
+        } else {
+          const h2 = document.getElementById("save_error");
+          h2.style.visibility = "visible";
+          data.error.map(error => (h2.innerHTML += error.msg + "\n"));
+          setTimeout(function() {
+            h2.style.visibility = "hidden";
+          }, 5000);
+        }
+      });
+  };
+
   render() {
     return (
       <div>
         <h1>Add Trains Page </h1>
-        <form id="form" action="">
+        <form id="form" onSubmit={this.submit}>
           <label htmlFor="name">Name : </label>
-          <input type="text" name="name" id="train_name" />
+          <input
+            type="text"
+            name="name"
+            id="train_name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
           <label htmlFor="train_no">Train No : </label>
-          <input type="number" name="train_no" id="train_no" />
+          <input
+            type="number"
+            name="train_no"
+            id="train_no"
+            value={this.state.train_no}
+            onChange={this.handleChange}
+          />
           <label htmlFor="available_tiers">Available Tiers : </label>
           <div>
             <input
@@ -16,6 +113,7 @@ class Train extends Component {
               name="available_tiers"
               id="available_tiers"
               value="1A"
+              onChange={this.handleChange}
             />
             1A
             <input
@@ -23,6 +121,7 @@ class Train extends Component {
               name="available_tiers"
               id="available_tiers"
               value="2A"
+              onChange={this.handleChange}
             />
             2A
             <input
@@ -30,6 +129,7 @@ class Train extends Component {
               name="available_tiers"
               id="available_tiers"
               value="3A"
+              onChange={this.handleChange}
             />
             3A
             <input
@@ -37,6 +137,7 @@ class Train extends Component {
               name="available_tiers"
               id="available_tiers"
               value="SL"
+              onChange={this.handleChange}
             />
             SL
             <input
@@ -44,6 +145,7 @@ class Train extends Component {
               name="available_tiers"
               id="available_tiers"
               value="CC"
+              onChange={this.handleChange}
             />
             CC
           </div>
@@ -54,6 +156,7 @@ class Train extends Component {
               name="departing_days"
               id="departing_days"
               value="mon"
+              onChange={this.handleChange}
             />
             Monday
             <br />
@@ -62,6 +165,7 @@ class Train extends Component {
               name="departing_days"
               id="departing_days"
               value="tue"
+              onChange={this.handleChange}
             />
             Tuesday
             <br />
@@ -70,6 +174,7 @@ class Train extends Component {
               name="departing_days"
               id="departing_days"
               value="wed"
+              onChange={this.handleChange}
             />
             Wednesday
             <br />
@@ -78,6 +183,7 @@ class Train extends Component {
               name="departing_days"
               id="departing_days"
               value="thu"
+              onChange={this.handleChange}
             />
             Thursday
             <br />
@@ -86,6 +192,7 @@ class Train extends Component {
               name="departing_days"
               id="departing_days"
               value="fri"
+              onChange={this.handleChange}
             />
             Friday
             <br />
@@ -94,6 +201,7 @@ class Train extends Component {
               name="departing_days"
               id="departing_days"
               value="sat"
+              onChange={this.handleChange}
             />
             Saturday
             <br />
@@ -102,27 +210,70 @@ class Train extends Component {
               name="departing_days"
               id="departing_days"
               value="sun"
+              onChange={this.handleChange}
             />
             Sunday
             <br />
           </div>
           <label htmlFor="route">Select Route</label>
-          <select name="route" id="route">
-            <option value=""></option>
-          </select>
+          <select name="route" id="route" onChange={this.handleChange}></select>
           <label htmlFor="depart_time">departure time : </label>
-          <input type="time" name="depart_time" id="depart_time" />
+          <input
+            type="time"
+            name="depart_time"
+            id="depart_time"
+            value={this.state.depart_time}
+            onChange={this.handleChange}
+          />
           <label htmlFor="arrival_time">arrival time : </label>
-          <input type="time" name="arrival_time" id="arrival_time" />
+          <input
+            type="time"
+            name="arrival_time"
+            id="arrival_time"
+            value={this.state.arrival_time}
+            onChange={this.handleChange}
+          />
           <label htmlFor="coach_seats">coach seats : </label>
-          <input type="number" name="coach_seats" id="coach_seats" />
+          <input
+            type="number"
+            name="coach_seats"
+            id="coach_seats"
+            value={this.state.coach_seats}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="total_coaches">Total Coaches : </label>
+          <input
+            type="number"
+            name="total_coaches"
+            id="total_coaches"
+            value={this.state.total_coaches}
+            onChange={this.handleChange}
+          />
           <label htmlFor="total_seats">total_seats : </label>
-          <input type="number" name="total_seats" id="total_seats" />
+          <input
+            type="number"
+            name="total_seats"
+            id="total_seats"
+            value={this.state.total_seats}
+            onChange={this.handleChange}
+          />
           <label htmlFor="ticket_cost">Ticket Cost : </label>
-          <input type="number" name="ticket_cost" id="ticket_cost" />
+          <input
+            type="number"
+            name="ticket_cost"
+            id="ticket_cost"
+            value={this.state.ticket_cost}
+            onChange={this.handleChange}
+          />
           <div />
           <input type="submit" value="Save" />
         </form>
+        <h2 id="train_saved" style={{ visibility: "hidden" }}>
+          Train saved to database...
+        </h2>
+        <h2 id="save_error" style={{ visibility: "hidden" }}>
+          Errors :{" "}
+        </h2>
       </div>
     );
   }
